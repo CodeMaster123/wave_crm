7f0f3d06af734568b7b9a53d59835484235de1c3
vivek varade <varade.vivek.123@gmail.com>
2013-04-04 10:12:10 +0530

    team lead, sales executive seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-
-
-User.create(
-    [
+User.create([
             {:email => "admin@gmail.com", :password => "admin123", :password_confirmation => "admin123", :account_type => "1"},
             {:email => "team_leader@gmail.com", :password => "team_leader123", :password_confirmation => "team_leader123", :account_type => "2"},
-            {:email => "sales_executive@gmail.com", :password => "sales_executive123", :password_confirmation => "sales_executive123", :account_type => "3"},
-            {:email => "sales_executive1@gmail.com", :password => "sales_executive1123", :password_confirmation => "sales_executive1123", :account_type => "3"}
+            {:email => "sales_executive1@gmail.com", :password => "sales_executive123", :password_confirmation => "sales_executive123", :account_type => "3"},
+            {:email => "sales_executive2@gmail.com", :password => "sales_executive1123", :password_confirmation => "sales_executive1123", :account_type => "3"},
+            {:email => "team_leader2@gmail.com", :password => "team_leader123", :password_confirmation => "team_leader123", :account_type => "2"},
+            {:email => "sales_executive3@gmail.com", :password => "sales_executive123", :password_confirmation => "sales_executive123", :account_type => "3"},
+            {:email => "sales_executive4@gmail.com", :password => "sales_executive1123", :password_confirmation => "sales_executive1123", :account_type => "3"}
+])
 
+TeamLeader.create([
+                  {:user_id => 2, :employee_id => 2},
+                  {:user_id => 5, :employee_id => 5}
+])
 
-    ]
-)
+SalesExecutive.create([
+                      {:team_leader_id =>1, :user_id =>3, :employee_id =>3},
+                      {:team_leader_id =>1, :user_id =>4, :employee_id =>4},
+                      {:team_leader_id =>2, :user_id =>6, :employee_id =>6},
+                      {:team_leader_id =>2, :user_id =>7, :employee_id =>7}
+])
