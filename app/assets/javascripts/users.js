f3954cabcb48becd971fee699ffae5833da661d7
vivek varade <varade.vivek.123@gmail.com>
2013-04-05 17:11:10 +0530

    jquery loaded at start, team-leader hidden in users#new form

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,6 @@
+        $('.account_type').change(function() {
+            if ($('.account_type').val() == 2)
+            {
+                $('.hide').fadeIn('hide').removeClass('hide');
+            }
+        });
