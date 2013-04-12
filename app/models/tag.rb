b027048a2a6f9596701ee04fa5be7a314b18e4d2
vivek varade <varade.vivek.123@gmail.com>
2013-04-12 15:59:00 +0530

    taxonomy added

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,14 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
   has_many :taggings
   has_many :contacts, through: :taggings
+
+  def tag_list_string
+      @tag_array =Array.new
+      @i = 0
+      Tag.all.each do  |tag|
+          @tag_array[@i] = tag.name
+          @i = @i+1
+      end
+      @tag_string = @tag_array.join(',')
+  end
 end
