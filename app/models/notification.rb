50c2767868dbd92dca36788bcade1e8f542b77b6
vivek varade <varade.vivek.123@gmail.com>
2013-05-07 18:03:02 +0530

    resque scheduler working properly, notifications being set in redis

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,16 +7,18 @@ class Notification < ActiveRecord::Base
   validates :contact_id, :presence => true
   validates :company_id, :presence => true
 
-  def sms_send(contact_number, body)
+  def sms_send
+      puts self.body
+      #puts self.contact.mobile_no
       @sms_gateway_username = "wave"
       @sms_gateway_password = "1492407050"
       @sms_sender_name = "wave"
-      @sms_receiver_number = contact_number #8888884083
-      @sms_message = URI::encode(body)
+      @sms_receiver_number = self.contact.mobile_no #contact_number #8888884083
+      @sms_message = URI::encode(self.body)
       @sms_api_url = "http://bulksms.mysmsmantra.com:8080/WebSMS/SMSAPI.jsp?username=#{@sms_gateway_username}&password=#{@sms_gateway_password}&sendername=#{@sms_sender_name}&mobileno=#{@sms_receiver_number}&message=#{@sms_message}"
 
       #if cur_request.length > 1000
-      Resque.enqueue(SmsScheduler, @sms_api_url)
+      Resque.enqueue_at(self.notification_time, SmsScheduler, @sms_api_url)
       #end
 
       #if request.length < cur_request.length
