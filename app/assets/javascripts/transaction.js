5115a821d73ebc84e92389cad6f0e3ae96f7efc3
vijaygaikwad1988 <vijaygaikwad1988@gmail.com>
2013-05-07 17:38:39 +0530

    not changes

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -2,12 +2,12 @@ $('.contact_type').change(function() {
     if ($('.contact_type').val() == 1)
 {
     $('.existing_contact').fadeIn('hide').removeClass('hide');
-    $('.new_contact').fadeOut('hide').addClass('hide');
+    $('.new_contact').fadeOut('hide').addClass('hide').$('hide').remove();
 }
 else
 if ($('.contact_type').val() == 2)
 {
-    $('.existing_contact').fadeOut('hide').addClass('hide');
+    $('.existing_contact').fadeOut('hide').addClass('hide').$('hide').remove();
     $('.new_contact').fadeIn('hide').removeClass('hide');
 }
 });
