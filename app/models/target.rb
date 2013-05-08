b6ce665a2fe108ceae5a2f780ffd2e2be5fc2826
vivek varade <varade.vivek.123@gmail.com>
2013-05-08 11:43:31 +0530

    sales_executive, team_leader's full name

--- app/models/target.rb
+++ app/models/target.rb
@@ -6,4 +6,9 @@ class Target < ActiveRecord::Base
   attr_accessible :achived, :amount, :start_target_date, :targetable_id, :targetable_type, :company_id, :end_target_date
 
   validates :company_id, :presence => true
+
+  def full_name
+      @user = self.targetable.user
+      "#{@user.first_name} #{@user.last_name}"
+  end
 end
