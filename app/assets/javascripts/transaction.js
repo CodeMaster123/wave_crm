8189be3d074c88df7aea667d5932df37e1d844bd
vivek varade <varade.vivek.123@gmail.com>
2013-05-08 17:55:51 +0530

    transaction saved for new contact

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -1,13 +1,24 @@
 $('.contact_type').change(function() {
     if ($('.contact_type').val() == 1)
-    {
-        $('.existing_contact').fadeIn('hide').removeClass('hide');
-        $('.new_contact').fadeOut('hide').addClass('hide');
-    }
-    else
-    if ($('.contact_type').val() == 2)
-    {
-        $('.existing_contact').fadeOut('hide').addClass('hide');
-        $('.new_contact').fadeIn('hide').removeClass('hide');
-    }
+{
+    $('.existing_contact').fadeIn('hide').removeClass('hide');
+    $('.new_contact').fadeOut('hide').addClass('hide');
+    $fields = $('.fields').clone();
+    $('.fields').remove();
+}
+else
+if ($('.contact_type').val() == 2)
+{
+    if($('.fields').length == 0)
+{
+    $('#new_transaction').append($fields);
+    $('.existing_contact').fadeOut('hide').addClass('hide');
+    $('.new_contact').fadeIn('hide').removeClass('hide');
+}
+else
+{
+    $('.existing_contact').fadeOut('hide').addClass('hide');
+    $('.new_contact').fadeIn('hide').removeClass('hide');
+}
+}
 });
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,7 +6,7 @@ class Transaction < ActiveRecord::Base
     attr_accessible :amount, :transaction_time, :company_id, :contact_type
 
     accepts_nested_attributes_for :contacts, :allow_destroy => true
-    attr_accessible :contact_attributes, :address, :first_name, :landline_no, :last_name, :latitude, :lead_id, :longitude, :middle_name, :mobile_no, :contact_type
+    attr_accessible :contacts_attributes, :address, :first_name, :landline_no, :last_name, :latitude, :lead_id, :longitude, :middle_name, :mobile_no, :contact_type
 
     validates :company_id, :presence => true
 
