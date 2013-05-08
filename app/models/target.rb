f1c32c7745aadc931d6c1fa677fb4cf0a2a72027
vijaygaikwad1988 <vijaygaikwad1988@gmail.com>
2013-05-08 14:36:19 +0530

    bootstrap nad validation added in target

--- app/models/target.rb
+++ app/models/target.rb
@@ -5,7 +5,13 @@ class Target < ActiveRecord::Base
 
   attr_accessible :achived, :amount, :start_target_date, :targetable_id, :targetable_type, :company_id, :end_target_date
 
+  validates :achived, :presence => true, :numericality => true
   validates :company_id, :presence => true
+  validates :amount, :presence => true, :numericality => true
+  validates :start_target_date, :presence => true
+  validates :targetable_id, :presence => true
+  validates :targetable_type, :presence => true
+  validates :end_target_date, :presence => true
 
   def full_name
       @user = self.targetable.user
