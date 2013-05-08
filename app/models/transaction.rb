074ade2c7cae6776953de9a8e476be87d6e11cd8
vivek varade <varade.vivek.123@gmail.com>
2013-05-08 16:17:54 +0530

    transactions nested_form working again

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,18 +1,20 @@
 class Transaction < ActiveRecord::Base
     has_many :transaction_field_values
-    has_one :contact, :as => :contactable
+    has_many :contacts, :as => :contactable
     belongs_to :company
 
     attr_accessible :amount, :transaction_time, :company_id, :contact_type
 
-    accepts_nested_attributes_for :contact, :allow_destroy => true
+    accepts_nested_attributes_for :contacts, :allow_destroy => true
     attr_accessible :contact_attributes, :address, :first_name, :landline_no, :last_name, :latitude, :lead_id, :longitude, :middle_name, :mobile_no, :contact_type
-
 
     validates :company_id, :presence => true
 
     def full_name
-        @user = self.contact
-        "#{@user.first_name.capitalize} #{@user.middle_name.capitalize} #{@user.last_name.capitalize}"
+        @contacts_list = String.new
+        self.contacts.each do |contact|
+            @contacts_list = @contacts_list + "#{contact.first_name.capitalize} #{contact.middle_name.capitalize} #{contact.last_name.capitalize}"
+        end
+            @contacts_list
     end
 end
