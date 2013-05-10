d2a8ae19488210399f7ab791e1f19baad0a4f45b
vivek varade <varade.vivek.123@gmail.com>
2013-05-10 17:44:40 +0530

    inventories, inventory_addition validation added

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,7 @@
 class Inventory < ActiveRecord::Base
     belongs_to :product
-  attr_accessible :product_id, :quantity
+    attr_accessible :product_id, :quantity
+
+    validates :product_id, :presence => true
+    validates :quantity, :presence => true
 end
