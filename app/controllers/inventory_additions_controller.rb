e1451912c7527d84250991309522626bbdc366de
vivek varade <varade.vivek.123@gmail.com>
2013-05-11 10:59:34 +0530

    inventory addition synced with inventory

--- app/controllers/inventory_additions_controller.rb
+++ app/controllers/inventory_additions_controller.rb
@@ -47,6 +47,8 @@ class InventoryAdditionsController < ApplicationController
 
     respond_to do |format|
       if @inventory_addition.save
+          @inventory = Inventory.where(:product_id => @inventory_addition.product_id).first
+          @inventory.update_attributes(:quantity => @inventory.quantity + @inventory_addition.quantity)
         format.html { redirect_to @inventory_addition, notice: 'Inventory addition was successfully created.' }
         format.json { render json: @inventory_addition, status: :created, location: @inventory_addition }
       else
@@ -63,25 +65,28 @@ class InventoryAdditionsController < ApplicationController
     @products = Company.find(current_user.company_id).products
 
     respond_to do |format|
-      if @inventory_addition.update_attributes(params[:inventory_addition])
-        format.html { redirect_to @inventory_addition, notice: 'Inventory addition was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @inventory_addition.errors, status: :unprocessable_entity }
-      end
+        @update_difference = params[:inventory_addition][:quantity].to_i - @inventory_addition.quantity
+        if @inventory_addition.update_attributes(params[:inventory_addition])
+            @inventory = Inventory.where(:product_id => @inventory_addition.product_id).first
+            @inventory.update_attributes(:quantity => (@inventory.quantity + @update_difference))
+            format.html { redirect_to @inventory_addition, notice: 'Inventory addition was successfully updated.' }
+            format.json { head :no_content }
+        else
+            format.html { render action: "edit" }
+            format.json { render json: @inventory_addition.errors, status: :unprocessable_entity }
+        end
     end
   end
 
   # DELETE /inventory_additions/1
   # DELETE /inventory_additions/1.json
   def destroy
-    @inventory_addition = InventoryAddition.find(params[:id])
-    @inventory_addition.destroy
+      @inventory_addition = InventoryAddition.find(params[:id])
+      @inventory_addition.destroy
 
-    respond_to do |format|
-      format.html { redirect_to inventory_additions_url }
-      format.json { head :no_content }
-    end
+      respond_to do |format|
+          format.html { redirect_to inventory_additions_url }
+          format.json { head :no_content }
+      end
   end
 end
