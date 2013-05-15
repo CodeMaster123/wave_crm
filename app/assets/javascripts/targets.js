d809d3e0552f251a71240de7e48a4fc7552942f7
vijaygaikwad1988 <vijaygaikwad1988@gmail.com>
2013-05-15 10:28:12 +0530

    before improve target.js

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -1,26 +1,13 @@
 $('.user_type').change(function() {
     if ($('.user_type').val() == "TeamLeader")
-    {
-        $('.team_leaders').fadeIn('hide').removeClass('hide');
-        $('.sales_executives').fadeOut('hide').addClass('hide');
-        $fields = $('.sales_executives').clone();
-        $('.sales_executives').remove();
-    }
-    else
-    if ($('.user_type').val() == "SalesExecutive")
-    {
-        if($('.sales_executive').length == 0)
-        {
-             $('#sales_executives').append($fields)
-             $('.team_leaders').fadeOut('hide').addClass('hide');
-             $('.sales_executives').fadeIn('hide').removeClass('hide');
-        }
-        else
-        {
-            $('.team_leaders').fadeOut('hide').addClass('hide');
-            $('.sales_executives').fadeIn('hide').removeClass('hide');
-        }
-    }
+{
+    $('.team_leaders').fadeIn('hide').removeClass('hide');
+    $('.sales_executives').fadeOut('hide').addClass('hide');
+}
+else
+if ($('.user_type').val() == "SalesExecutive")
+{
+    $('.team_leaders').fadeOut('hide').addClass('hide');
+    $('.sales_executives').fadeIn('hide').removeClass('hide');
+}
 });
-
-
