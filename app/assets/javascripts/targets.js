7f503373b690f6a8886fee379b90010082101450
vijaygaikwad1988 <vijaygaikwad1988@gmail.com>
2013-05-15 10:34:54 +0530

    target.js improved and target#create cant take targatable_id error remove

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -1,13 +1,15 @@
 $('.user_type').change(function() {
     if ($('.user_type').val() == "TeamLeader")
-{
-    $('.team_leaders').fadeIn('hide').removeClass('hide');
-    $('.sales_executives').fadeOut('hide').addClass('hide');
-}
-else
-if ($('.user_type').val() == "SalesExecutive")
-{
-    $('.team_leaders').fadeOut('hide').addClass('hide');
-    $('.sales_executives').fadeIn('hide').removeClass('hide');
-}
+    {
+        $('.team_leaders').fadeIn('hide').removeClass('hide');
+        $('.sales_executives').fadeOut('hide').addClass('hide');
+        $fields = $('.sales_executives').clone();
+        $('.sales_executives').remove();
+    }
+    else
+        if ($('.user_type').val() == "SalesExecutive")
+        {
+            $('.team_leaders').fadeOut('hide').addClass('hide');
+            $('.sales_executives').fadeIn('hide').removeClass('hide');
+        }
 });
