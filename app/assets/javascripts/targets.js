6258be745a06ea05e2d8b81382a69ba2531c0365
vijaygaikwad1988 <vijaygaikwad1988@gmail.com>
2013-05-15 12:51:09 +0530

    target.js fully improved

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -9,7 +9,20 @@ $('.user_type').change(function() {
     else
         if ($('.user_type').val() == "SalesExecutive")
         {
-            $('.team_leaders').fadeOut('hide').addClass('hide');
-            $('.sales_executives').fadeIn('hide').removeClass('hide');
+            if($('.sales_executives').length == 0)
+            {
+                $('.target').append($fields);
+                $('.team_leaders').fadeOut('hide').addClass('hide');
+                $('.sales_executives').fadeIn('hide').removeClass('hide');
+            }
+            else
+            {
+                $('.team_leaders').fadeOut('hide').addClass('hide');
+                $('.sales_executives').fadeIn('hide').removeClass('hide');
+            }
         }
 });
+
+
+
+
