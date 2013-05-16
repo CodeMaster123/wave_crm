1641549528ec611b1fc1b664a2a058cc6e3d259d
vivek varade <varade.vivek.123@gmail.com>
2013-05-16 12:38:58 +0530

    leads javascript refactored

--- app/assets/javascripts/leads.js
+++ app/assets/javascripts/leads.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
         dateFormat: 'yy-mm-dd',
     timeFormat: 'hh:mm tt z'
     });
+
+    $('.search-c').searchbox({
+        url: '/lead_search.html',
+        param: 'q',
+        dom_id: '#search-data',
+        delay: 100
+    })
 });
