6c0bb002ab763b5bec95823cd6a6eed7c673f59a
vivek varade <varade.vivek.123@gmail.com>
2013-05-17 14:22:03 +0530

    jquery removed from include

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
 //= require jquery_ujs
 
 //= require_self
-//= require jquery-1.9.1.min
 //= require jquery-ui-1.10.2.custom.min
 //= require jquery-ui-timepicker-addon
 //= require jquery-ui-sliderAccess
