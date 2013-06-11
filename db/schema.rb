b18ab5d9acc846f9ffb88ba1957873d16d6406e9
vivek varade <varade.vivek.123@gmail.com>
2013-06-11 18:14:51 +0530

    mobile_no changed to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -298,17 +298,17 @@ ActiveRecord::Schema.define(:version => 20130603103304) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address"
-    t.integer  "mobile_no"
+    t.integer  "mobile_no",              :limit => 8
     t.integer  "account_type"
     t.integer  "company_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",                  :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
