411f1aa1d05d5a58a08673042b5dc2dd9f79f6d9
vivek varade <varade.vivek.123@gmail.com>
2013-06-11 13:09:48 +0530

    superuser can see all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
     # GET /users
     # GET /users.json
     def index
-        @company = Company.where(:id => current_user.company_id).first
-        @users = User.all
-        @users = @company.users.paginate(:page => params[:page], :per_page => 15)
+        unless current_user.account_type == 4
+            @company = Company.where(:id => current_user.company_id).first
+            @users = @company.users.paginate(:page => params[:page], :per_page => 15)
+        else
+            @users = User.all
+        end
 
         respond_to do |format|
             format.html # index.html.erb
