3471134402cd22d73a3370106fc1ca74ad9a205b
vivek varade <varade.vivek.123@gmail.com>
2013-06-28 14:11:06 +0530

    follow-ups removed

--- app/models/follow_up.rb
+++
@@ -1,7 +0,0 @@
-class FollowUp < ActiveRecord::Base
-    belongs_to :leads
-    belongs_to :company
-    attr_accessible :description, :follow_up_time, :lead_id, :company_id
-
-    validates :company_id, :presence => true
-end
