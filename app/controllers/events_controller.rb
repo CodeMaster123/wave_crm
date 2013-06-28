35e4979701b3830bb761484324fe39467989dcc8
vivek varade <varade.vivek.123@gmail.com>
2013-06-28 14:55:29 +0530

    Tests passing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,19 +11,9 @@ class EventsController < ApplicationController
         end
     end
 
-
-    # GET /events
-    # GET /events.xml
     def index
-        # full_calendar will hit the index method with query parameters
-        # 'start' and 'end' in order to filter the results for the
-        # appropriate month/week/day.  It should be possiblt to change
-        # this to be starts_at and ends_at to match rails conventions.
-        # I'll eventually do that to make the demo a little cleaner.
-      @company = Company.where(:id => current_user.company_id).first
-      @events = Event.scoped
-        #@events = @events.after(params['start']) if (params['start'])
-        #@events = @events.before(params['end']) if (params['end'])
+        @company = Company.where(:id => current_user.company_id).first
+        @events = Event.scoped
         @events = Event.where(:user_id => current_user.id, :company_id => current_user.company_id).paginate(:page => params[:page], :per_page => 15)
         respond_to do |format|
             format.html # index.html.erb
@@ -63,11 +53,11 @@ class EventsController < ApplicationController
     # POST /events
     # POST /events.xml
     def create
-      @company = Company.where(:id => current_user.company_id).first
-      @event = @company.events.new(params[:event])
-      @event.company_id = @company.id
-       @event.user_id = current_user.id
-      respond_to do |format|
+        @company = Company.where(:id => current_user.company_id).first
+        @event = @company.events.new(params[:event])
+        @event.company_id = @company.id
+        @event.user_id = current_user.id
+        respond_to do |format|
             if @event.save
                 format.html { redirect_to :events, :notice => 'Event was successfully created.' }
                 format.xml  { render :xml => @event, :status => :created, :location => @event }
@@ -85,8 +75,8 @@ class EventsController < ApplicationController
     # it on the week or day view), this method will be called to update the values.
     # viv la REST!
     def update
-      @company = Company.where(:id => current_user.company_id).first
-      @event = @company.events.find(params[:id])
+        @company = Company.where(:id => current_user.company_id).first
+        @event = @company.events.find(params[:id])
 
         respond_to do |format|
             if @event.update_attributes(params[:event])
@@ -115,11 +105,11 @@ class EventsController < ApplicationController
 
     def search
         #if current_user.account_type == 1
-            unless params[:q].empty?
-                @events = Event.search params[:q], :with => {:company_id => current_user.company_id}
-            else
-                @events = Event.where(:company_id => current_user.company_id)
-            end
+        unless params[:q].empty?
+            @events = Event.search params[:q], :with => {:company_id => current_user.company_id}
+        else
+            @events = Event.where(:company_id => current_user.company_id)
+        end
         #end
         respond_to do |format|
             format.html
