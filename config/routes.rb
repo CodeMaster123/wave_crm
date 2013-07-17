267ab5e330a25ff39686e35d3f74aefc3b6cb0cc
vivek varade <varade.vivek.123@gmail.com>
2013-07-17 16:46:13 +0530

    routes sorted

--- config/routes.rb
+++ config/routes.rb
@@ -27,48 +27,55 @@ WaveCrm::Application.routes.draw do
     resources :events
     resources :partial_payments
 
+    #Sinatra apps
     mount_sextant if Rails.env.development?
     mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
     mount Resque::Server, :at => '/resque_panel'
 
+    #Search functionality
     match 'contact_search' => 'contacts#search'
     match 'lead_search' => 'leads#search'
     match 'tag_search' => 'tags#search'
     match 'notification_search' => 'notifications#search'
     match 'event_search' => 'events#search'
 
+    #Reporting and intelligence
     match 'transaction_graph' => 'transactions#graph'
-
-    match 'calendar' => "calendar#index"
-    match 'sales_executive_leads/:id1' => 'sales_executives#index'
+    match 'map_index' => 'contacts#map_index'
     match 'target_forecast' => 'team_leaders#target_forecast'
+    match 'targets/index/:type' => 'targets#index'
+    match 'old_target' => 'targets#old_target'
 
+    #Notifications and events
+    match 'calendar' => "calendar#index"
     match 'noticed' => 'notifications#noticed'
     match 'notifications_to_all' => 'notifications#notifications_to_all'
-    match 'map_index' => 'contacts#map_index'
+
+    #Used in transactions controller
     match 'transactions/new/:id1/:matured_by/:executive_type' => 'transactions#new'
     match 'transactions/mature/:id1/:matured_by/:executive_type' => 'transactions#new'
-    match 'old_target' => 'targets#old_target'
     match 'transaction_invoice/:id' => 'transactions#invoice' #, :defaults => {:format => 'pdf'}
+    match 'get_partial_payments/:transaction_id' => 'transactions#get_partial_payments'
+    match 'create_partial_payment' => 'partial_payments#create_partial_payment'
 
+    #Tuple classification in index method
     match 'contacts/index/:type' => 'contacts#index'
     match 'accounts/index/:type' => 'accounts#index'
-
+    match 'leads/index/:type' => 'leads#index', :as => :lead_change
+    match 'leads/index/:team_leader' => 'leads#index'
     match 'notifications/index/:type' => 'notifications#index', :as => :n_change
+    match 'sales_executive_leads/:id1' => 'sales_executives#index'
 
+    #Used in Leads controller
+    match 'postpone_lead' => 'leads#postpone_lead'
+
+    #Used in Leads#show
     match 'create_call_logs' => 'call_logs#create_log'
     match 'create_events' => 'events#create_event'
     match 'create_notification' => 'notifications#create_notification'
     match 'create_contact' => 'contacts#create_contact'
     match 'create_transaction' => 'transactions#create_transaction'
-    match 'get_partial_payments/:transaction_id' => 'transactions#get_partial_payments'
-    match 'create_partial_payment' => 'partial_payments#create_partial_payment'
-
-    match 'leads/index/:type' => 'leads#index', :as => :lead_change
-    match 'leads/index/:team_leader' => 'leads#index'
     match 'change_owner' => 'leads#change_owner'
-    match 'postpone_lead' => 'leads#postpone_lead'
 
-    match 'targets/index/:type' => 'targets#index'
     root :to => 'leads#index'
 end
