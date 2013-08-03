835180c55cdfd6131c0dfa9e962e505ebcd8267b
vivek varade <varade.vivek.123@gmail.com>
2013-08-03 16:59:42 +0530

    Tagging functionality removed

--- app/assets/javascripts/tags.js
+++
@@ -1,9 +0,0 @@
-    $(document).ready(function(){
-
-            $('.search-c').searchbox({
-url: '/tag_search.html',
-param: 'q',
-dom_id: '#search-data',
-delay: 100,
-});
-});
--- app/models/tagging.rb
+++
@@ -1,6 +0,0 @@
-class Tagging < ActiveRecord::Base
-  belongs_to :tag
-  belongs_to :contact
-
-  attr_accessible :tag_id, :contact_id
-end
