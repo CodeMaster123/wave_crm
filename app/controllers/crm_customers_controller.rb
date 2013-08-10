35760eb4ee6e1a197ed641d4d58984b12da871e1
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 14:18:55 +0530

    crm_customers refactored

--- app/controllers/crm_customers_controller.rb
+++ app/controllers/crm_customers_controller.rb
@@ -6,70 +6,44 @@ class CrmCustomersController < ApplicationController
   def index
     @crm_customers = CrmCustomer.all
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @crm_customers }
-    end
+    respond_with @crm_customers
   end
 
   def show
-    @crm_customer = CrmCustomer.find(params[:id])
+      @crm_customer = CrmCustomer.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @crm_customer }
-    end
+      respond_with @crm_customer
   end
 
   def new
-    @crm_customer = CrmCustomer.new
-    @crm_customer.build_company
+      @crm_customer = CrmCustomer.new
+      @crm_customer.build_company
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @crm_customer }
-    end
+      respond_with @crm_customer
   end
 
   def edit
-    @crm_customer = CrmCustomer.find(params[:id])
+      @crm_customer = CrmCustomer.find(params[:id])
   end
 
   def create
-    @crm_customer = CrmCustomer.new(params[:crm_customer])
+      @crm_customer = CrmCustomer.new(params[:crm_customer])
 
-    respond_to do |format|
-      if @crm_customer.save
-        format.html { redirect_to @crm_customer, notice: 'Crm customer was successfully created.' }
-        format.json { render json: @crm_customer, status: :created, location: @crm_customer }
-      else
-        format.html { render "new" }
-        format.json { render json: @crm_customer.errors, status: :unprocessable_entity }
-      end
-    end
+      @crm_customer.save
+      respond_with @crm_customer
   end
 
   def update
-    @crm_customer = CrmCustomer.find(params[:id])
+      @crm_customer = CrmCustomer.find(params[:id])
 
-    respond_to do |format|
-      if @crm_customer.update_attributes(params[:crm_customer])
-        format.html { redirect_to @crm_customer, notice: 'Crm customer was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render "edit" }
-        format.json { render json: @crm_customer.errors, status: :unprocessable_entity }
-      end
-    end
+      @crm_customer.update_attributes(params[:crm_customer])
+      respond_with @crm_customer
   end
 
   def destroy
-    @crm_customer = CrmCustomer.find(params[:id])
-    @crm_customer.destroy
+      @crm_customer = CrmCustomer.find(params[:id])
+      @crm_customer.destroy
 
-    respond_to do |format|
-      format.html { redirect_to crm_customers_url }
-      format.json { head :no_content }
-    end
+      respond_with @crm_customer
   end
 end
