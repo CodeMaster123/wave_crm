ed87ad0467d347183f685cbf860c9f5b176ce2ef
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 15:16:55 +0530

    inventories_addition model refactored

--- app/controllers/inventory_additions_controller.rb
+++ app/controllers/inventory_additions_controller.rb
@@ -1,33 +1,25 @@
 class InventoryAdditionsController < ApplicationController
     before_filter :authenticate_user!
     filter_access_to :all
+    respond_to :html, :json
 
     def index
         @inventory_additions = InventoryAddition.all
 
-        respond_to do |format|
-            format.html # index.html.erb
-            format.json { render json: @inventory_additions }
-        end
+        respond_with @inventory_additions
     end
 
     def show
         @inventory_addition = InventoryAddition.find(params[:id])
 
-        respond_to do |format|
-            format.html # show.html.erb
-            format.json { render json: @inventory_addition }
-        end
+        respond_with @inventory_addition
     end
 
     def new
         @inventory_addition = InventoryAddition.new
-        @products = Company.find(current_user.company_id).products
+        @products = current_user.company.products
 
-        respond_to do |format|
-            format.html # new.html.erb
-            format.json { render json: @inventory_addition }
-        end
+        respond_with @inventory_addition
     end
 
     def edit
@@ -36,50 +28,27 @@ class InventoryAdditionsController < ApplicationController
     end
 
     def create
-        @inventory = Inventory.where(:product_id => params[:inventory_addition][:product_id]).first
+        @inventory = Inventory.find(params[:inventory_addition][:product_id])
+        @products = current_user.company.products
+        @inventory_addition = @inventory.inventory_additions.new(params[:inventory_addition])
 
-        @inventory_addition = InventoryAddition.new(params[:inventory_addition])
-        @inventory_addition.inventory_id = @inventory.id
-
-        @products = Company.find(current_user.company_id).products
-
-        respond_to do |format|
-            if @inventory_addition.save
-                @inventory.update_attributes(:quantity => @inventory.quantity + @inventory_addition.quantity)
-                format.html { redirect_to inventories_path, notice: 'Inventory addition was successfully created.' }
-                format.json { render json: @inventory_addition, status: :created, location: @inventory_addition }
-            else
-                format.html { render "new" }
-                format.json { render json: @inventory_addition.errors, status: :unprocessable_entity }
-            end
-        end
+        @inventory_addition.save
+        respond_with @inventory_addition, :location => inventories_path
     end
 
     def update
         @inventory_addition = InventoryAddition.find(params[:id])
-        @products = Company.find(current_user.company_id).products
+        @products = current_user.company.products
+        @inventory_addition.update_difference = params[:inventory_addition][:quantity].to_i - @inventory_addition.quantity
+        @inventory_addition.update_attributes(params[:inventory_addition])
 
-        respond_to do |format|
-            @update_difference = params[:inventory_addition][:quantity].to_i - @inventory_addition.quantity
-            if @inventory_addition.update_attributes(params[:inventory_addition])
-                @inventory = Inventory.where(:product_id => @inventory_addition.product_id).first
-                @inventory.update_attributes(:quantity => (@inventory.quantity + @update_difference))
-                format.html { redirect_to inventories_path, notice: 'Inventory addition was successfully updated.' }
-                format.json { head :no_content }
-            else
-                format.html { render "edit" }
-                format.json { render json: @inventory_addition.errors, status: :unprocessable_entity }
-            end
-        end
+        respond_with @inventory_addition, :location => inventories_path
     end
 
     def destroy
         @inventory_addition = InventoryAddition.find(params[:id])
         @inventory_addition.destroy
 
-        respond_to do |format|
-            format.html { redirect_to inventory_additions_url }
-            format.json { head :no_content }
-        end
+        respond_with @inventory_addition
     end
 end
--- app/models/inventory_addition.rb
+++ app/models/inventory_addition.rb
@@ -2,12 +2,25 @@ class InventoryAddition < ActiveRecord::Base
     include PublicActivity::Model
     tracked owner: ->(controller, model) {controller && controller.current_user}
 
-
     belongs_to :product
 
-    attr_accessible :addition_date, :product_id, :quantity, :company_id, :inventory_id
+    attr_accessible :addition_date, :product_id, :quantity, :company_id, :inventory_id, :update_product_id, :update_difference
+    attr_accessor :update_product_id, :update_difference
 
     validates :product_id, :presence => true
     validates :quantity, :presence => true
     validates :addition_date, :presence => true
+
+    after_update :update_inventory
+    after_create :create_inventory
+
+    def update_inventory
+        @inventory = Inventory.find(self.product_id)
+        @inventory.update_attributes(:quantity => @inventory.quantity + self.update_difference)
+    end
+
+    def create_inventory
+        @inventory = Inventory.find(self.product_id)
+        @inventory.update_attributes(:quantity => @inventory.quantity + self.quantity)
+    end
 end
