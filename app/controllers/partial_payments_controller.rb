50762915da7241841da408f8154c85cf8efec7b4
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 14:20:02 +0530

    partial payment refactored

--- app/controllers/partial_payments_controller.rb
+++ app/controllers/partial_payments_controller.rb
@@ -36,32 +36,20 @@ class PartialPaymentsController < ApplicationController
     def update
         @partial_payment = PartialPayment.find(params[:id])
 
-        respond_to do |format|
-            if @partial_payment.update_attributes(params[:partial_payment])
-                format.html { redirect_to @partial_payment, notice: 'Partial payment was successfully updated.' }
-                format.json { head :no_content }
-            else
-                format.html { render action: "edit" }
-                format.json { render json: @partial_payment.errors, status: :unprocessable_entity }
-            end
-        end
+        @partial_payment.update_attributes(params[:partial_payment])
+        respond_with @partial_payment
     end
 
     def destroy
         @partial_payment = PartialPayment.find(params[:id])
         @partial_payment.destroy
 
-        respond_to do |format|
-            format.html { redirect_to partial_payments_url }
-            format.json { head :no_content }
-        end
+        respond_with @partial_payment
     end
 
     def create_partial_payment
         @partial_payment = PartialPayment.create(:amount_paid => params[:amount], :transaction_id => params[:transaction_id])
 
-        respond_to do |format|
-            format.html{render :nothing => true}
-        end
+        respond_with @partial_payment
     end
 end
