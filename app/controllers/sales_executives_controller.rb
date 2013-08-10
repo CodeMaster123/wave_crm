a5c624d30927cf0a91afe0e13e59f2b7a124932c
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 13:50:56 +0530

    sales executives controller improved

--- app/controllers/sales_executives_controller.rb
+++ app/controllers/sales_executives_controller.rb
@@ -1,81 +1,57 @@
 class SalesExecutivesController < ApplicationController
     before_filter :authenticate_user!
     filter_access_to :all
+    respond_to :html, :json
 
     def index
-        @company = Company.where(:id => current_user.company_id).first
+        @company = current_user.company
         if current_user.account_type == 1
             @sales_executives = @company.sales_executives.all
         elsif current_user.account_type == 2
             @sales_executives = User.find(current_user.id).team_leader.sales_executives
         end
 
-        respond_to do |format|
-            format.html # index.html.erb
-            format.json { render json: @sales_executives }
-        end
+        respond_with @sales_executives
     end
 
     def show
         @sales_executive = SalesExecutive.find(params[:id])
 
-        respond_to do |format|
-            format.html # show.html.erb
-            format.json { render json: @sales_executive }
-        end
+        respond_with @sales_executive
     end
 
     def new
         @sales_executive = SalesExecutive.new
 
-        respond_to do |format|
-            format.html # new.html.erb
-            format.json { render json: @sales_executive }
-        end
+        respond_with @sales_executive
     end
 
     def edit
         @sales_executive = SalesExecutive.find(params[:id])
+
+        respond_with @sales_executive
     end
 
     def create
-        @company = Company.where(:id => current_user.company_id).first
+        @company = current_user.company
         @sales_executive = @company.sales_executives.new(params[:sales_executive])
-        @sales_executive.company_id = @company.id
 
-        respond_to do |format|
-            if @sales_executive.save
-                format.html { redirect_to :sales_executives, notice: 'Sales executive was successfully created.' }
-                format.json { render json: @sales_executive, status: :created, location: @sales_executive }
-            else
-                format.html { render "new" }
-                format.json { render json: @sales_executive.errors, status: :unprocessable_entity }
-            end
-        end
+        @sales_executive.save
+        respond_with @sales_executive
     end
 
     def update
         @company = Company.where(:id => current_user.company_id).first
         @sales_executive = @company.sales_executives.find(params[:id])
 
-        respond_to do |format|
-            if @sales_executive.update_attributes(params[:sales_executive])
-                format.html { redirect_to @sales_executive, notice: 'Sales executive was successfully updated.' }
-                format.json { head :no_content }
-            else
-                format.html { render "edit" }
-                format.json { render json: @sales_executive.errors, status: :unprocessable_entity }
-            end
-        end
+        @sales_executive.update_attributes(params[:sales_executive])
+        respond_with @sales_executive
     end
 
     def destroy
         @sales_executive = SalesExecutive.find(params[:id])
         @sales_executive.destroy
 
-        respond_to do |format|
-            format.html { redirect_to sales_executives_url }
-            format.json { head :no_content }
-        end
+        respond_with @sales_executive
     end
 end
