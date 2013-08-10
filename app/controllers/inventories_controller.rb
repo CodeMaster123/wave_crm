9fcdd7722aa12af2231292db2b3fbdb08e54ffb7
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 14:04:28 +0530

    inventories refactored

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,79 +1,55 @@
 class InventoriesController < ApplicationController
   before_filter :authenticate_user!
   filter_access_to :all
+  respond_to :html, :json, :xml
+
 
   def index
-    @inventories = current_user.company.inventories.all
+      @inventories = current_user.company.inventories.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @inventories }
-    end
+      respond_with @inventories
   end
 
   def show
-    @inventory = Inventory.find(params[:id])
-    @inventory_additions = @inventory.inventory_additions
-    @inventory_removal = @inventory.product.product_transactions
+      @inventory = Inventory.find(params[:id])
+      @inventory_additions = @inventory.inventory_additions
+      @inventory_removal = @inventory.product.product_transactions
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @inventory }
-    end
+      respond_with @inventory
   end
 
   def new
-    @inventory = Inventory.new
-    @products = Company.find(current_user.company_id).products
+      @inventory = Inventory.new
+      @products = current_user.company.products
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @inventory }
-    end
+      respond_with @inventory
   end
 
   def edit
-    @inventory = Inventory.find(params[:id])
-    @products = Company.find(current_user.company_id).products
+      @inventory = Inventory.find(params[:id])
+      @products = current_user.company.products
   end
 
   def create
-    @inventory = Inventory.new(params[:inventory])
-    @products = Company.find(current_user.company_id).products
+      @inventory = Inventory.new(params[:inventory])
+      @products = current_user.company.products
 
-    respond_to do |format|
-      if @inventory.save
-        format.html { redirect_to @inventory, notice: 'Inventory was successfully created.' }
-        format.json { render json: @inventory, status: :created, location: @inventory }
-      else
-        format.html { render "new" }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
-      end
-    end
+      @inventory.save
+      respond_with @inventory
   end
 
   def update
-    @inventory = Inventory.find(params[:id])
-    @products = Company.find(current_user.company_id).products
+      @inventory = Inventory.find(params[:id])
+      @products = current_user.company.products
 
-    respond_to do |format|
-      if @inventory.update_attributes(params[:inventory])
-        format.html { redirect_to @inventory, notice: 'Inventory was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render "edit" }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
-      end
-    end
+      @inventory.update_attributes(params[:inventory])
+      respond_with @inventory
   end
 
   def destroy
-    @inventory = Inventory.find(params[:id])
-    @inventory.destroy
+      @inventory = Inventory.find(params[:id])
+      @inventory.destroy
 
-    respond_to do |format|
-      format.html { redirect_to inventories_url }
-      format.json { head :no_content }
-    end
+      respond_with @inventory
   end
 end
