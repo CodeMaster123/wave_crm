7e42ef8bc69c2173a44cb453d93d607f2fce04dc
vivek varade <varade.vivek.123@gmail.com>
2013-08-10 14:01:45 +0530

    employees controller refactored

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,32 +1,24 @@
 class EmployeesController < ApplicationController
     before_filter :authenticate_user!
     filter_access_to :all
+    respond_to :html, :json
 
     def index
         @employees = Employee.all
 
-        respond_to do |format|
-            format.html # index.html.erb
-            format.json { render json: @employees }
-        end
+        respond_with @employees
     end
 
     def show
         @employee = Employee.find(params[:id])
 
-        respond_to do |format|
-            format.html # show.html.erb
-            format.json { render json: @employee }
-        end
+        respond_with @employee
     end
 
     def new
         @employee = Employee.new
 
-        respond_to do |format|
-            format.html # new.html.erb
-            format.json { render json: @employee }
-        end
+        respond_with @employee
     end
 
     def edit
@@ -36,38 +28,21 @@ class EmployeesController < ApplicationController
     def create
         @employee = Employee.new(params[:employee])
 
-        respond_to do |format|
-            if @employee.save
-                format.html { redirect_to :employees, notice: 'Employee was successfully created.' }
-                format.json { render json: @employee, status: :created, location: @employee }
-            else
-                format.html { render "new" }
-                format.json { render json: @employee.errors, status: :unprocessable_entity }
-            end
-        end
+        @employee.save
+        respond_with @employee
     end
 
     def update
         @employee = Employee.find(params[:id])
 
-        respond_to do |format|
-            if @employee.update_attributes(params[:employee])
-                format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
-                format.json { head :no_content }
-            else
-                format.html { render "edit" }
-                format.json { render json: @employee.errors, status: :unprocessable_entity }
-            end
-        end
+        @employee.update_attributes(params[:employee])
+        respond_with @employee
     end
 
     def destroy
         @employee = Employee.find(params[:id])
         @employee.destroy
 
-        respond_to do |format|
-            format.html { redirect_to employees_url }
-            format.json { head :no_content }
-        end
+        respond_with @employee
     end
 end
