3a460e5ea01e9805a13cbaf76d2fbd42c62ed5f9
vivek varade <varade.vivek.123@gmail.com>
2013-08-12 15:58:22 +0530

    validated mobile_no's uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
     validates :first_name, :presence => true
     validates :last_name, :presence => true
     validates :address, :presence => true
-    validates :mobile_no, :numericality => true
+    validates :mobile_no, :numericality => true, :uniqueness => true
     validates :email, :uniqueness => true
     validates :password, :presence => true
     validates :password_confirmation, :presence => true
