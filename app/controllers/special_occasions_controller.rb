b61becceacb95650f0f012400f535ef1bd5e8296
vivek varade <varade.vivek.123@gmail.com>
2013-08-12 11:19:09 +0530

    special occasions refactored

--- app/controllers/special_occasions_controller.rb
+++ app/controllers/special_occasions_controller.rb
@@ -1,32 +1,24 @@
 class SpecialOccasionsController < ApplicationController
     before_filter :authenticate_user!
     filter_access_to :all
+    respond_to :html, :json
 
     def index
         @special_occasions = SpecialOccasion.all
 
-        respond_to do |format|
-            format.html # index.html.erb
-            format.json { render json: @special_occasions }
-        end
+        respond_with @special_occasions
     end
 
     def show
         @special_occasion = SpecialOccasion.find(params[:id])
 
-        respond_to do |format|
-            format.html # show.html.erb
-            format.json { render json: @special_occasion }
-        end
+        respond_with @special_occasion
     end
 
     def new
         @special_occasion = SpecialOccasion.new
 
-        respond_to do |format|
-            format.html # new.html.erb
-            format.json { render json: @special_occasion }
-        end
+        respond_with @special_occasion
     end
 
     def edit
@@ -35,39 +27,22 @@ class SpecialOccasionsController < ApplicationController
 
     def create
         @special_occasion = SpecialOccasion.new(params[:special_occasion])
+        @special_occasion.save
 
-        respond_to do |format|
-            if @special_occasion.save
-                format.html { redirect_to @special_occasion, notice: 'Special occasion was successfully created.' }
-                format.json { render json: @special_occasion, status: :created, location: @special_occasion }
-            else
-                format.html { render "new" }
-                format.json { render json: @special_occasion.errors, status: :unprocessable_entity }
-            end
-        end
+        respond_with @special_occasion
     end
 
     def update
         @special_occasion = SpecialOccasion.find(params[:id])
+        @special_occasion.update_attributes(params[:special_occasion])
 
-        respond_to do |format|
-            if @special_occasion.update_attributes(params[:special_occasion])
-                format.html { redirect_to @special_occasion, notice: 'Special occasion was successfully updated.' }
-                format.json { head :no_content }
-            else
-                format.html { render "edit" }
-                format.json { render json: @special_occasion.errors, status: :unprocessable_entity }
-            end
-        end
+        respond_with @special_occasion
     end
 
     def destroy
         @special_occasion = SpecialOccasion.find(params[:id])
         @special_occasion.destroy
 
-        respond_to do |format|
-            format.html { redirect_to special_occasions_url }
-            format.json { head :no_content }
-        end
+        respond_with @special_occasion
     end
 end
