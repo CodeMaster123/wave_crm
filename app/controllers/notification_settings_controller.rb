53714687589b9cd1294021f9bb3a32a43e0b5b62
vivek varade <varade.vivek.123@gmail.com>
2013-08-12 15:18:04 +0530

    notification_settings improved

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -3,36 +3,10 @@ class NotificationSettingsController < ApplicationController
     filter_access_to :all
     respond_to :html, :json
 
-    def index
-        @notification_settings = NotificationSetting.all
-
-        respond_with @notification_settings
-    end
-
-    def show
-        @notification_setting = NotificationSetting.find(params[:id])
-
-        respond_with @notification_setting
-    end
-
-    def new
-        @notification_setting = NotificationSetting.new
-
-        respond_with @notification_setting
-    end
-
     def edit
-        @notification_setting = NotificationSetting.find(params[:id])
         @notification_setting = NotificationSetting.find(params[:id])
         @notification_setting.update_attributes(:notification_flag => false)
 
-        respond_with @notification_setting
-    end
-
-    def create
-        @notification_setting = NotificationSetting.new(params[:notification_setting])
-
-        @notification_setting.save
         respond_with @notification_setting
     end
 
@@ -40,13 +14,6 @@ class NotificationSettingsController < ApplicationController
         @notification_setting = NotificationSetting.find(params[:id])
 
         @notification_setting.update_attributes(params[:notification_setting])
-        respond_with @notification_setting
-    end
-
-    def destroy
-        @notification_setting = NotificationSetting.find(params[:id])
-        @notification_setting.destroy
-
         respond_with @notification_setting
     end
 end
