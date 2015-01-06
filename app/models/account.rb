80b8b6537c7ef1188a5163eb718acc854fe5a4cc
vivek varade <varade.vivek.123@gmail.com>
2015-01-06 12:17:34 +0530

    accounts validation updated

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,13 +3,11 @@ class Account < ActiveRecord::Base
 
   belongs_to :lead
   belongs_to :company
-  #has_one :account
   has_many :contacts
   has_many :transactions
 
   validates :account_name, :presence => true
-  #validates :account_name, :presence => true
-  #validates :account_name, :presence => true
-  validates :phone_number, :presence => true
+  validates :phone_number, :presence => true, length: {minimum: 5, maximum: 10}
   validates :billing_address, :presence => true
+  validates :website, :allow_blank => false, format: {with: /\./, message: 'Not a valid domain name'}
 end
