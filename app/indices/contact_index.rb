3ff18ed52005e6fc43c6dc369e1cf11a94bf8015
vivek varade <varade.vivek.123@gmail.com>
2015-01-07 10:29:45 +0530

    indices temporarily removed

--- app/indices/contact_index.rb
+++
@@ -1,7 +0,0 @@
-ThinkingSphinx::Index.define :contact, :with => :active_record do
-  indexes first_name, last_name
-  has :company_id
-
-  set_property :min_infix_len => 1
-  #set_property :min_prefix_len => 1
-end
--- app/indices/event_index.rb
+++
@@ -1,9 +0,0 @@
-ThinkingSphinx::Index.define :event, :with => :active_record do
-  indexes title
-  indexes description
-
-  has company_id
-
-  set_property :min_infix_len => 1
-end
-
