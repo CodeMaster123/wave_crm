a6eac2303b1e4ad518606c1ef3c78ef7eb806b81
vivek varade <varade.vivek.123@gmail.com>
2015-01-30 10:51:35 +0530

    leads filter implemented

--- app/assets/javascripts/leads/controllers/leads_index_controller.js
+++ app/assets/javascripts/leads/controllers/leads_index_controller.js
@@ -1,8 +1,17 @@
-leads_app.controller('LeadsIndexController',function($scope, Restangular, $routeParams){
+leads_app.controller('LeadsIndexController',function($scope, Restangular, $routeParams, $location){
   $scope.init = function(){
     Leads = Restangular.all('leads')
     Leads.getList().then(function(leads){
-      $scope.leads = leads
+      $scope.leads = leads;
+      if($scope.temp_lead==undefined)
+        $scope.status_filter();
+    })
+  }
+
+  $scope.status_filter = function(lead_status){
+    console.log(lead_status);
+    $scope.temp_leads = _.filter($scope.leads, function(lead){
+      return lead.lead_status == status
     })
   }
 
