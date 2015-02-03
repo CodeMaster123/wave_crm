c25154d38811e2bc800a58094d06cfcfd787a81a
vivek varade <varade.vivek.123@gmail.com>
2015-02-03 12:33:35 +0530

    UI related issues fixed

--- app/assets/javascripts/calendar/services/calendar_app.js
+++ app/assets/javascripts/calendar/services/calendar_app.js
@@ -1,7 +1,7 @@
 var calendar_app = angular.module('calendar_app', ['ngRoute', 'ng-rails-csrf', 'ui.bootstrap', 'ui.calendar']);
 calendar_app.config(function($routeProvider){
   $routeProvider
-  .when('/calendar',{
+  .when('/index',{
     templateUrl: '/templates/calendar/calendar.html',
     controller: 'CalendarIndexController'
   })
