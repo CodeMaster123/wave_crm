278e5c9f694f683c4f511979789f228d34e0e2c4
Vijay Gaikwad <vijaygaikwad1988@gmail.com>
2015-02-05 15:57:43 +0530

    call log spec improved

--- spec/controllers/call_logs_controller_spec.rb
+++ spec/controllers/call_logs_controller_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe CallLogsController, :type => :controller do
     @call_log = create(:call_log)
   end
 
+  it 'should POST create method' do
+    expect {
+      post :create, call_log: FactoryGirl.attributes_for(:call_log)
+    }.to change(CallLog, :count).by(1)
+  end
+
 end
 
